ac4b40717013ee94bc56ccd72fbec5022f07fc43
Nhor2 <117581117+Nhor2@users.noreply.github.com>
2025-08-08 09:31:22 +0200

    GoldbachForte
    Aggiunta progetto GoldbachForte con sorgenti e risorse

---
+++ My Project/AssemblyInfo.vb
@@ -0,0 +1,32 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' Le informazioni generali relative a un assembly sono controllate dal seguente 
+' set di attributi. Modificare i valori di questi attributi per modificare le informazioni
+' associate a un assembly.
+
+' Controllare i valori degli attributi degli assembly
+
+<Assembly: AssemblyTitle("Sedici-9")>
+<Assembly: AssemblyDescription("Calcolo automatico.")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("Sedici-9")>
+<Assembly: AssemblyCopyright("Copyright ©  2025")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'Se il progetto viene esposto a COM, il GUID seguente verrà usato come ID del typelib
+<Assembly: Guid("2b3674e6-220f-4e48-b9a0-4d63567a5dd6")>
+
+' Le informazioni sulla versione di un assembly sono costituite dai seguenti quattro valori:
+'
+'      Versione principale
+'      Versione secondaria
+'      Numero di build
+'      Revisione
+'
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
